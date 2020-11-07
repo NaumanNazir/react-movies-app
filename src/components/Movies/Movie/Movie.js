3eda035468cc3e660d518c2b47ac7b899cf011c5
Nauman Nazir <nauman001@yahoo.com>
2020-11-07 23:56:20 +0100

    updated

--- src/components/Movies/Movie/Movie.js
+++ src/components/Movies/Movie/Movie.js
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from "react";
 
-import classes from './Movie.module.css'
+import classes from "./Movie.module.css";
 
-const IMAGE_API = "https://image.tmdb.org/t/p/w1280"
+const IMAGE_API = "https://image.tmdb.org/t/p/w1280";
 
 function Movie(props) {
-  const {title, poster_path, vote_average} = props.movie
-   
+  const { title, poster_path, release_date } = props.movie;
+
   return (
     <div className={classes.Movie}>
       <div className={classes.ImageContainer}>
         <a href="#0">
-          <img src={IMAGE_API + poster_path} alt={title}/>
+          <img src={IMAGE_API + poster_path} alt={title} />
         </a>
-        <h3> hello </h3>
       </div>
       <div className={classes.Content}>
-
+        <h3> {title} </h3>
+        <small> {release_date} </small>
       </div>
     </div>
-  )
+  );
 }
 
-export default Movie
+export default Movie;
--- src/containers/MovieFetcher/MovieFetcher.js
+++ src/containers/MovieFetcher/MovieFetcher.js
@@ -1,75 +1,77 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import axios from 'axios'
-import MenuBar from '../../components/Navigation/MenuBar/MenuBar'
-import Banner from '../../components/Banner/Banner'
-import Movies from '../../components/Movies/Movies'
-import Spinner from '../../components/Spinner/Spinner'
+import axios from "axios";
+import MenuBar from "../../components/Navigation/MenuBar/MenuBar";
+import Banner from "../../components/Banner/Banner";
+import Movies from "../../components/Movies/Movies";
+import Spinner from "../../components/Spinner/Spinner";
 
-const API_KEY = 'eb73f2959b63226925762febe27af005'
-const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&page=1`
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${API_KEY}&query=`
+const API_KEY = "eb73f2959b63226925762febe27af005";
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&page=1`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${API_KEY}&query=`;
 
 class App extends Component {
   state = {
     movies: [],
     loading: false,
-    searchInput: '',
-  }
-  
+    searchInput: "",
+  };
+
   // we had two functions with the same behavior, so we made a new function.
   // responsible for fetching movies.
 
   getMovies = (API) => {
-    this.setState({loading: true})
-    axios.get(API)
-      .then(response => {
+    this.setState({ loading: true });
+    axios
+      .get(API)
+      .then((response) => {
+        // console.log(response.data.results);
         this.setState({
           movies: response.data.results,
           loading: false,
-        })
+        });
       })
-      .catch(error => {
-        console.log(error)
-      })
-  }
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   componentDidMount() {
-    this.getMovies(FEATURED_API)
-    
+    this.getMovies(FEATURED_API);
   }
 
   handleOnSubmit = (e) => {
-    e.preventDefault()
-    
-    const search = this.state.searchInput
+    e.preventDefault();
+
+    const search = this.state.searchInput;
     if (!!search) {
-      this.getMovies(SEARCH_API+search)
+      this.getMovies(SEARCH_API + search);
       this.setState({
-        searchInput: ''
-      })
+        searchInput: "",
+      });
     }
-  }
+  };
 
   handleOnChange = (e) => {
     this.setState({
-      searchInput: e.target.value
-    })
-  }
+      searchInput: e.target.value,
+    });
+  };
 
   render() {
-    let renderMovies = 
-      (this.state.loading) 
-        ? <Spinner />
-        : <Movies movies={this.state.movies} />
+    let renderMovies = this.state.loading ? (
+      <Spinner />
+    ) : (
+      <Movies movies={this.state.movies} />
+    );
 
     return (
       <div>
         <MenuBar />
         <Banner
-          searchInput={this.state.searchInput} 
+          searchInput={this.state.searchInput}
           handleSubmit={this.handleOnSubmit}
-          handleChange={this.handleOnChange} 
+          handleChange={this.handleOnChange}
         />
         {renderMovies}
       </div>
@@ -78,10 +80,6 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-
 
 // This is the second way to fetch api.
 
@@ -94,12 +92,12 @@ export default App;
 //     })
 //     .catch(error => {
 //       console.log(error)
-//     }) 
+//     })
 // }
 
 // handleOnSubmit = (e) => {
 //   e.preventDefault()
-  
+
 //   const search = this.state.searchInput
 //   if (!!search) {
 //     same we did in componentDidMount
