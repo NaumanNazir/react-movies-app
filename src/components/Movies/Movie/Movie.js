4832645e7cab6e27c239eedf9bedf05fc4cf6359
Nauman Nazir <nauman001@yahoo.com>
2020-11-08 15:27:02 +0100

    updated

--- src/components/Movies/Movie/Movie.js
+++ src/components/Movies/Movie/Movie.js
@@ -1,11 +1,14 @@
 import React from "react";
 
+import dayjs from "dayjs";
+import { CgOptions } from "react-icons/cg";
 import classes from "./Movie.module.css";
 
 const IMAGE_API = "https://image.tmdb.org/t/p/w1280";
 
 function Movie(props) {
   const { title, poster_path, release_date } = props.movie;
+  const releaseDate = dayjs(release_date).format("MMM DD, YYYY");
 
   return (
     <div className={classes.Movie}>
@@ -13,10 +16,13 @@ function Movie(props) {
         <a href="#0">
           <img src={IMAGE_API + poster_path} alt={title} />
         </a>
+        <a className={classes.Tooltip} href="#0">
+          <CgOptions />
+        </a>
       </div>
       <div className={classes.Content}>
         <h3> {title} </h3>
-        <small> {release_date} </small>
+        <small> {releaseDate} </small>
       </div>
     </div>
   );
